1bf2ddf45c7b5a0781c99d1bd23e03517a5dd373
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2025-02-24 10:37:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var string1 = "Life is good";
+var string2 = 'Life is good';
+var string3 = `Life is good`;
+
+var quote = 'They are called "The avengers"';
+console.log("Output of 2nd - "+quote);
+console.log("Number of Characters = "+ quote.length);
+
+console.log(quote[5]);
+
+var string4 = 'If you won\'t have fruits/fruit juice and vegetables/ green leafy vegetables, then you won\'t be healthy.';
+console.log(string4);
